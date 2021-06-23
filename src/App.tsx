3d2e1c9fc7489c2c92bd859237598948f235833f
codehg <hugoaraliveira@gmail.com>
2021-06-23 17:13:23 -0300

    Configura autenticacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,16 @@
-import { createContext, useState } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom';
-
 import { NewRoom } from './pages/NewRoom';
 import { Home } from './pages/Home';
-import { auth, firebase } from './services/firebase';
+import { AuthContextProvider } from './contexts/AuthContext'
 
-export const AuthContext = createContext({} as any);
 
 function App() {
-  const [user, setUser] = useState();
-
-  function signInWithGoogle() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    auth.signInWithPopup(provider).then(result => {
-      if (result.user) {
-        const { displayName, photoURL, uid } = result.user
-
-        if (!displayName || !photoURL) {
-          throw new Error('Missing information from Google Account.');
-        }
-
-        setUser({
-          id: uid,
-          name: displayName,
-          avatar: photoURL
-        })
-      }
-    })
-  }
-
   return (
     <BrowserRouter>
-      <AuthContext.Provider value={{ value, setValue }}>
+      <AuthContextProvider>
         <Route path="/" exact component={Home} />
         <Route path="/rooms/new" component={NewRoom} />
-      </AuthContext.Provider>
+      </AuthContextProvider>
     </BrowserRouter >
   );
 }
