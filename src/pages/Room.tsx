a28b787ab33ad153245bd0f8db1b81bb4a34a95e
codehg <hugoaraliveira@gmail.com>
2021-07-23 12:21:00 -0300

    Estrutura lista de perguntas

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -7,62 +7,22 @@ import { useAuth } from '../hooks/useAuth';
 import { database } from '../services/firebase';
 import { FormEvent, useState } from 'react';
 import { useEffect } from 'react';
+import { Question } from '../components/Question';
+import { useRoom } from '../hooks/useRoom';
 
-type FirebaseQuestions = Record<string, {
-  author: {
-    name: string;
-    avatar: string;
-  }
-  content: string;
-  isAnswered: boolean;
-  isHighLighted: boolean;
-}>
 
 type RoomParams = {
   id: string;
 }
 
-type Question = {
-  id: string;
-  author: {
-    name: string;
-    avatar: string;
-  }
-  content: string;
-  isAnswered: boolean;
-  isHighLighted: boolean;
-}
 
 export function Room() {
   const { user } = useAuth();
   const params = useParams<RoomParams>();
-  const roomId = params.id;
   const [newQuestion, setNewQuestion] = useState('');
-  const [questions, setQuestions] = useState<Question[]>([]);
-  const [title, setTitle] = useState('');
+  const roomId = params.id;
 
-
-  useEffect(() => {
-    const roomRef = database.ref(`rooms/${roomId}`);
-
-    roomRef.on('value', room => {
-      const databaseRoom = room.val();
-      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
-
-      const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
-        return {
-          id: key,
-          content: value.content,
-          author: value.author,
-          isHighLighted: value.isHighLighted,
-          isAnswered: value.isAnswered,
-        }
-      })
-
-      setTitle(databaseRoom.title);
-      setQuestions(parsedQuestions);
-    })
-  }, [roomId]);
+  const { title, questions } = useRoom(roomId)
 
   async function handleSendQuestion(event: FormEvent) {
     event.preventDefault();
@@ -123,7 +83,17 @@ export function Room() {
             <Button type="submit" disabled={!user}>Enviar pergunta</Button>
           </div>
         </form>
-        {JSON.stringify(questions)}
+        <div className="question-list">
+          {questions.map(question => {
+            return (
+              <Question
+                key={question.id}
+                content={question.content}
+                author={question.author}
+              />
+            );
+          })}
+        </div>
       </main>
     </div>
   );
